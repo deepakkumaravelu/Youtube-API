e59fa1dfd84e1d0afd8ba0230dc646b59f1b2777
deepak <deepak1122003kumar@gmail.com>
2023-10-24 20:20:18 +0530

    trying cat api deleted youtube api

--- youtubeapi/src/App.js
+++ youtubeapi/src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
-
-const API_KEY='AIzaSyAFYd9rPaVlpQlhcytFxotavCzX3tLvOuc';
-
+const Url="https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_Dk7EGKeq4rae2BpD8SW1zLmop8jTAyD3AYi19s0NMzzaJSmLC5SbT6B0tHisomeO";
+fetch(Url)
+.then(response => response.json())
+.then(data => {
+  // Extract the channel ID from the search results
+  const img = data;
+  if (img) {
+    const channelId = img[0].url;
+    document.querySelector(".imm").src=channelId;
+  } else {
+    console.log(`not found.`);
+  }
+})
+.catch(error => {
+  console.error(`Error searching for the channel: ${error}`);
+});
 function App() {
- 
   return (
     <div className="App">
-    <h1>view console</h1>
+    <img src="" className="imm" ></img>
     </div>
   );
 }
-
 export default App;
