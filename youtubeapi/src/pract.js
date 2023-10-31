1dd739659ff8fbdb80dfab407ad69ed0812f9753
deepak <deepak1122003kumar@gmail.com>
2023-10-31 21:42:55 +0530

    practiced to use require()

--- youtubeapi/src/pract.js
+++ youtubeapi/src/pract.js
@@ -1,5 +1,15 @@
-const fs=require("fs");
-fs.writeFile("message.txt","hello world",(err) => {
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your name? ', (name) => {
+  fs.writeFile("message.txt", name, (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
-  }); 
+    rl.close();
+  });
+});
