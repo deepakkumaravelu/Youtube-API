f2688f277fceaf132f0854dd45447b385d6e99b2
deepak <deepak1122003kumar@gmail.com>
2023-11-02 19:30:25 +0530

    creating my first api

--- youtubeapi/src/pract.js
+++ youtubeapi/src/pract.js
@@ -1,15 +1,26 @@
-const fs = require("fs");
-const readline = require("readline");
+// const fs = require("fs");
+// const readline = require("readline");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-rl.question('What is your name? ', (name) => {
-  fs.writeFile("message.txt", name, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-    rl.close();
-  });
-});
+// rl.question('What is your name? ', (name) => {
+//   fs.writeFile("message.txt", name, (err) => {
+//     if (err) throw err;
+//     console.log('The file has been saved!');
+//     rl.close();
+//   });
+// });
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
